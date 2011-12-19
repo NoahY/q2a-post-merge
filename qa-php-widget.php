f672b4b62abb751419d885b1bff7b4c0dc308e74
NoahY <noahy57@gmail.com>
2011-12-19 19:41:56 +0530

    update

--- qa-php-widget.php
+++ qa-php-widget.php
@@ -69,20 +69,17 @@
                                         $acount,$to
                                     );
 
-                                    $table_exists = qa_db_read_one_value(qa_db_query_sub("SHOW TABLES LIKE '^postmeta'"),true);
-                                    if(!$table_exists) {
-                                        qa_db_query_sub(
-                                            'CREATE TABLE IF NOT EXISTS ^postmeta (
-                                            meta_id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-                                            post_id bigint(20) unsigned NOT NULL,
-                                            meta_key varchar(255) DEFAULT \'\',
-                                            meta_value longtext,
-                                            PRIMARY KEY (meta_id),
-                                            KEY post_id (post_id),
-                                            KEY meta_key (meta_key)
-                                            ) ENGINE=MyISAM  DEFAULT CHARSET=utf8'
-                                        );			
-                                    }
+                                    qa_db_query_sub(
+                                        'CREATE TABLE IF NOT EXISTS ^postmeta (
+                                        meta_id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                                        post_id bigint(20) unsigned NOT NULL,
+                                        meta_key varchar(255) DEFAULT \'\',
+                                        meta_value longtext,
+                                        PRIMARY KEY (meta_id),
+                                        KEY post_id (post_id),
+                                        KEY meta_key (meta_key)
+                                        ) ENGINE=MyISAM  DEFAULT CHARSET=utf8'
+                                    );			
                                     
                                     qa_db_query_sub(
                                         "INSERT INTO ^postmeta (post_id,meta_key,meta_value) VALUES (#,'merged_with',#)",
